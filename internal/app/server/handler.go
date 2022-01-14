6565b4dfd29046f251badc974e9cd97c4c102f0d
Oleg Fadeev <o.fadeev@live.ru>
2022-01-14 13:18:14 +0300

    rebase

--- internal/app/server/handler.go
+++ internal/app/server/handler.go
@@ -7,36 +7,34 @@ import (
 
 func (s *Server) handlePickup() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		hL, hK := s.getHeaderForAuth(r.Header)
+		e := false
 
-		if hL != "" && hK != "" {
-			e := false
-			idU, err := s.pkg.store.GetUserLogin(hL, hK)
-			if err != nil || idU < 0 {
-				s.pkg.logger.Error(err.Error())
-				http.Error(w, s.msgErrorNoLogin(), http.StatusUnauthorized)
-			} else {
-				if z := r.URL.Query().Get("zip"); z != "" {
-					l, err := s.pkg.store.GetPointsFromZip(z)
-					if err != nil {
-						e = true
-						s.pkg.logger.Error(fmt.Sprintf("Request zip: %s. ", z), err.Error())
-					}
-					w.Write(l)
-				} else if c := r.URL.Query().Get("city"); c != "" {
-					l, err := s.pkg.store.GetPointsFromCity(c)
-					if err != nil {
-						e = true
-						s.pkg.logger.Error(fmt.Sprintf("Request sity: %s. ", c), err.Error())
-					}
-					w.Write(l)
+		login, err := s.auth(r.Header)
+
+		if err != nil {
+			s.pkg.logger.Error(err.Error())
+			http.Error(w, s.msgErrorNoLogin(), http.StatusUnauthorized)
+		} else if len(login) > 0 {
+			if z := r.URL.Query().Get("zip"); z != "" {
+				l, err := s.pkg.store.GetPointsFromZip(z)
+				if err != nil {
+					e = true
+					s.pkg.logger.Error(fmt.Sprintf("Request zip: %s. ", z), err.Error())
 				}
+				w.Write(l)
+			} else if c := r.URL.Query().Get("city"); c != "" {
+				l, err := s.pkg.store.GetPointsFromCity(c)
+				if err != nil {
+					e = true
+					s.pkg.logger.Error(fmt.Sprintf("Request sity: %s. ", c), err.Error())
+				}
+				w.Write(l)
 			}
 			if !e {
-				s.pkg.logger.Info(s.msgReqPointsSuccess(hL, r.RemoteAddr))
+				s.pkg.logger.Info(s.msgReqPointsSuccess(login, r.RemoteAddr))
 			}
 		} else {
-			s.pkg.logger.Error(s.msgReqPointsFail(hL, r.RemoteAddr))
+			s.pkg.logger.Error(s.msgReqPointsFail(login, r.RemoteAddr))
 			http.Error(w, s.msgErrorNoLogin(), http.StatusUnauthorized)
 		}
 	}
--- internal/app/server/serverfunc.go
+++ internal/app/server/serverfunc.go
@@ -5,6 +5,20 @@ import (
 	"net/http"
 )
 
-func (s *Server) getHeaderForAuth(header http.Header) (string, string) {
-	return fmt.Sprintf("%s", header.Get("Login")), fmt.Sprintf("%s", header.Get("Key"))
+func (s *Server) auth(header http.Header) (string, error) {
+	login, key := fmt.Sprintf("%s", header.Get("Login")), fmt.Sprintf("%s", header.Get("Key"))
+
+	if login != "" && key != "" {
+		idU, err := s.pkg.store.GetUserLogin(login, key)
+		if err != nil || idU < 0 {
+			return "", err
+		} else {
+			return login, nil
+		}
+	}
+	return "", nil
+}
+
+func (s *Server) getData() {
+
 }
